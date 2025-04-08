446b986346cb898da5b00aaef500f484cb8b8ffc
Bab Codes <chanitebrandon@gmail.com>
2025-04-08 10:51:12 +0200

    commit form register

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,20 +1,22 @@
 $(document).ready(function() {
-    $("input[type='radio']").change(saveData);
+    
 
     $("#society, #name, #firstname").on("blur", saveData);
-
+    $("input[type='radio']").change(saveData);
     function saveData() {
         var results = {};
 
         // Récupération des données avec le name
-        $("input[type='radio']:checked").each(function() {
-            results[$(this).attr("name")] = $(this).val();
-        });
+        
 
         // Récupération des données avec l'id
         results["society"] = $("#society").val().trim();
         results["name"] = $("#name").val().trim();
         results["firstname"] = $("#firstname").val().trim();
+
+        $("input[type='radio']:checked").each(function() {
+            results[$(this).attr("name")] = $(this).val();
+        });
 
         sessionStorage.setItem("quizResults", JSON.stringify(results));
         console.log("Données sauvegardées :", results);
